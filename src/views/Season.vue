d855d44d2b63fb153c0b02ad16d8cd0afdcd61b9
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-08-28 18:39:40 +0200

    season navigation

--- src/views/Season.vue
+++ src/views/Season.vue
@@ -1,7 +1,13 @@
 <template>
   <h1>
-    Season
+    {{t('season.title',{year:year})}}:
+    <Icon type="season" :name="seasonName" class="seasonIcon"/>
+    {{t(`seasonName.${seasonName}`)}}
   </h1>
+
+  <router-link v-if="nextButtonRouteTo" :to="nextButtonRouteTo" class="btn btn-primary btn-lg mt-3">
+    {{t('action.next')}}
+  </router-link>
 
   <FooterButtons :backButtonRouteTo="backButtonRouteTo" :endGameButtonType="endOfGame ? 'endGame' : 'abortGame'"/>
 </template>
@@ -13,12 +19,15 @@ import { useRoute } from 'vue-router'
 import { useStore } from '@/store'
 import FooterButtons from '@/components/structure/FooterButtons.vue'
 import NavigationState from '@/util/NavigationState'
+import Icon from '@/components/structure/Icon.vue'
+import Season from '@/services/enum/Season'
 
 export default defineComponent({
   name: 'RoundTurn',
   components: {
-    FooterButtons
-  },
+    FooterButtons,
+    Icon
+},
   setup() {
     const { t } = useI18n()
     const store = useStore()
@@ -32,14 +41,51 @@ export default defineComponent({
   },
   computed: {
     backButtonRouteTo() : string {
-      return ''
+      if (this.year == 1 && this.season == 1) {
+        return ''
+      }
+      else if (this.season == 1) {
+        return `/year/${this.year-1}/season/4`
+      }
+      else {
+        return `/year/${this.year}/season/${this.season-1}`
+      }
     },
     nextButtonRouteTo() : string {
-      return ''
+      if (this.endOfGame) {
+        return ''
+      }
+      else if (this.season == 4) {
+        return `/year/${this.year+1}/season/1`
+      }
+      else {
+        return `/year/${this.year}/season/${this.season+1}`
+      }
     },
     endOfGame() : boolean {
-      return false
+      return (this.year == 4 && this.season == 4)
+    },
+    seasonName() : string {
+      switch (this.season) {
+        case Season.SPRING:
+          return 'spring'
+        case Season.SUMMER:
+          return 'summer'
+        case Season.AUTUMN:
+          return 'autumn'
+        case Season.WINTER:
+          return 'winter'
+        default:
+          return ''
+      }
     }
   }
 })
 </script>
+
+<style lang="scss">
+.seasonIcon {
+  height: 2rem;
+  margin-top: -0.5rem;
+}
+</style>
