cb7b1a06e9dcdad2bc8c4063a84cfc40ce6fc40a
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-08-30 00:20:57 +0200

    mark player turns

--- src/components/turn/SeasonActions.vue
+++ src/components/turn/SeasonActions.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="accordion" id="botActionsAccordion">
-    <div class="accordion-item" v-for="bot in botCount" :key="bot">
-      <h2 class="accordion-header" :id="`bot${bot}Heading`">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="`#bot${bot}Collapse`" aria-expanded="true" :aria-controls="`bot${bot}Collapse`">
-          <PlayerColorDisplay :playerColor="botPlayerColors[bot-1]" class="playerColorIcon"/>
-          <b>{{t('season.actions.turn',{opponent:t(`opponent.${botOpponent[bot-1]}`)})}}</b>
+  <div class="accordion" id="actionsAccordion">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="playerHeading">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#playerCollapse" aria-expanded="false" aria-controls="playerCollapse"
+            @click="makeTurn(0)" :key="'player:'+turnDone[0]" :class="{done:turnDone[0]}">
+          <PlayerColorDisplay :playerColor="playerColors[0]" class="playerColorIcon"/>
+          <b>{{t('season.actions.playerTurn')}}</b>
+          <input class="form-check-input turn-done" type="checkbox" :value="true" v-model="turnDone[0]">
         </button>
       </h2>
-      <div :id="`bot${bot}Collapse`" class="accordion-collapse collapse" aria-labelledby="`bot${bot}Heading`" data-bs-parent="#botActionsAccordion">
+      <div id="playerCollapse" class="accordion-collapse collapse" aria-labelledby="playerHeading" data-bs-parent="#actionsAccordion">
+        <div class="accordion-body">
+          {{t('season.actions.playerTurnInfo')}}
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item" v-for="bot in botCount" :key="bot">
+      <h2 class="accordion-header" :id="`bot${bot}Heading`">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="`#bot${bot}Collapse`" aria-expanded="false" :aria-controls="`bot${bot}Collapse`"
+            @click="makeTurn(bot)" :key="bot+':'+turnDone[bot]" :class="{done:turnDone[bot]}">
+          <PlayerColorDisplay :playerColor="playerColors[bot]" class="playerColorIcon"/>
+          <b>{{t('season.actions.botTurn',{opponent:t(`opponent.${botOpponent[bot-1]}`)})}}</b>
+          <input class="form-check-input turn-done" type="checkbox" :value="true" v-model="turnDone[bot]">
+        </button>
+      </h2>
+      <div :id="`bot${bot}Collapse`" class="accordion-collapse collapse" :aria-labelledby="`bot${bot}Heading`" data-bs-parent="#actionsAccordion">
         <div class="accordion-body">
           <SeasonAction :card-deck="cardDeck[bot-1]" :difficultyLevel="difficultyLevel"/>
         </div>
@@ -51,6 +68,11 @@ export default defineComponent({
       required: true
     }
   },
+  data() {
+    return {
+      turnDone: [] as boolean[]
+    }
+  },
   computed: {
     difficultyLevel() : number {
       return this.navigationState.difficultyLevel
@@ -61,11 +83,16 @@ export default defineComponent({
     botOpponent() : Opponent[] {
       return this.navigationState.botOpponent
     },
-    botPlayerColors() : PlayerColor[] {
-      return this.navigationState.botPlayerColors
+    playerColors() : PlayerColor[] {
+      return this.navigationState.playerColors
     },
     cardDeck() : CardDeck[] {
       return this.navigationState.cardDeck as CardDeck[]
+    }
+  },
+  methods: {
+    makeTurn(index : number) : void {
+      this.turnDone[index] = true
     }
   }
 })
@@ -79,5 +106,12 @@ export default defineComponent({
     height: 1.5rem;
     margin-left: 0.25rem;
     margin-right: 0.25rem;
+  }
+  input.turn-done {
+    margin-top: 0rem;
+    margin-left: 0.5rem;
+  }
+  button.done {
+    color: #ccc;
   }
 </style>
