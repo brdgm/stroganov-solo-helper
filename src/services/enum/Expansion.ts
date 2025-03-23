7a8df7f05b583da2fa865534a3d6afbd0eeaeed3
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-03-23 22:46:36 +0100

    expansion selection

---
+++ src/services/enum/Expansion.ts
@@ -0,0 +1,7 @@
+enum Expansion {
+  TURUKHAN_CHIPMUNKS_MERCHANT = 'turukhan-chipmunks-merchant',
+  TURUKHAN_HUNTING_PLAN = 'turukhan-hunting-plan',
+  TURUKHAN_NEW_LANDSCAPE_TILES = 'turukhan-new-landscape-tiles',
+  TURUKHAN_DIPLOMACY = 'turukhan-diplomacy'
+}
+export default Expansion
